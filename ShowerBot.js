8e543be95ce18e2811f25294ceca68f01dcb608c
Ofir Levi <ofirl24@gmail.com>
2020-03-27 17:13:41 +0300

    test

--- ShowerBot.js
+++ ShowerBot.js
@@ -11,7 +11,7 @@ let bot;
 
 if (process.env.NODE_ENV === 'production') {
     bot = new Bot(token);
-    bot.setWebHook(process.env.HEROKU_URL + bot.token);
+    bot.setWebHook(process.env.BOT_WEBHOOK + bot.token);
 }
 else {
     bot = new Bot(token, { polling: true });
--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, "206.189.122.197", () => {
+var server = app.listen(process.env.PORT, "0.0.0.0", () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Web server started at http://%s:%s', host, port);
