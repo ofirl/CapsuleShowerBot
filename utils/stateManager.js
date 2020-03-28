8152f56ec228b1de1f4149cdf52058e7601bee2f
Ofir Levi <ofirl24@gmail.com>
2020-03-28 15:24:31 +0300

    fix

--- utils/stateManager.js
+++ utils/stateManager.js
@@ -28,9 +28,7 @@ function endBreak(bot, msg) {
     });
 
     if (queueUtils.queue.length < 2)
-        return;
-
-    bot.sendMessage(queueUtils.queue[1].id, `${queueUtils.queue[0].first_name || ""} ${queueUtils.queue[0].last_name || ""} ${queueUtils.queue[0].username ? `(@${queueUtils.queue[0].username})` : ""} is now going to the shower,\nYou can get ready, you are next `);
+        bot.sendMessage(queueUtils.queue[1].id, `${queueUtils.queue[0].first_name || ""} ${queueUtils.queue[0].last_name || ""} ${queueUtils.queue[0].username ? `(@${queueUtils.queue[0].username})` : ""} is now going to the shower,\nYou can get ready, you are next `);
 
     return true;
 }
